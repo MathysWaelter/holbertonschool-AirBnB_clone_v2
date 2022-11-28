a1219bc8e50842b68c4c8f2ca1ea4bcd917eac01
root <you@example.com>
2022-11-28 03:08:31 -0800

    print with flash and add readme and __init__

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""Import flask and print with method"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_HBNB():
     """print Hello HBNB"""
     return "<p>Hello HBNB!</p>"
