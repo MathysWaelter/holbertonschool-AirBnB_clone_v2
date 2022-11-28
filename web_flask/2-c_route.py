8ffcb95cecd2d6a3a23ff43a7ae515c1290237dd
root <you@example.com>
2022-11-28 04:39:01 -0800

    display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Import flask and print with method"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """print Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """print HBNB in /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def HBNB_text(text):
+    """print HBNB in /hbnb"""
+    texts = text.replace('_', ' ')
+    return texts
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
