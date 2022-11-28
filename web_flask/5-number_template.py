2a0aea1a25810674e41335b4353855cccb1d0a99
root <you@example.com>
2022-11-28 05:59:55 -0800

    display a HTML page only if n is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Import flask and print with method"""
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """print Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """print HBNB in /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def HBNB_text(text):
+    """print text variable and replace character"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text):
+    """print text variable and replace character"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """print n if is integer"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def template(n):
+    """print html page if is integer"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
