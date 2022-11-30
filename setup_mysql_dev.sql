d44280ec16e0f7c220b0abebcb64ab92b4b73048
root <you@example.com>
2022-11-30 06:54:19 -0800

    fork from TAIEB CHABINI

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,5 @@
--- Create new database
+-- a script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create new user
-CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY 'hbnb_dev_pwd';
--- Set all privilege for hbnb_dev_db
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- Set SELECT privilege for performance_schema DB
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
--- Save the changes
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,6 @@
--- Create new database
+-- a script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Create new user
-CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY 'hbnb_test_pwd';
--- Set all privilege for hbnb_test_db
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
--- Set SELECT privilege for performance_schema DB
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
--- Save the changes
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS performance_schema;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
