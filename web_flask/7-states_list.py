7d320fe7fe146f2c50d266b9259e07fd9f3f644f
root <you@example.com>
2022-12-02 05:40:10 -0800

    add new method for print state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,66 +1,23 @@
 #!/usr/bin/python3
 """Import flask and print with method"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from models import storage
-from models.state import State
+
 
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def hello_HBNB():
-    """print Hello HBNB"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def HBNB():
-    """print HBNB in /hbnb"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def HBNB_text(text):
-    """print text variable and replace character"""
-    text = text.replace('_', ' ')
-    return "C {}".format(text)
-
-
-@app.route("/python/", defaults={'text': 'is cool'})
-@app.route("/python/<text>", strict_slashes=False)
-def display_python(text):
-    """print text variable and replace character"""
-    text = text.replace('_', ' ')
-    return "Python {}".format(text)
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_n(n):
-    """print n if is integer"""
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def template(n):
-    """print html page if is integer"""
-    return render_template('5-number.html', n=n)
-
 @app.route("/states_list", strict_slashes=False)
-def state_list(n):
+def state_list(n=None):
     """return all state"""
-    state = storage.all(State).values()
-    return render_template("7-states_list.html", n=state)
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def even_or_odd(n):
-    """print html page if is integer"""
-    if (n % 2) == 0:
-        text = "{} is even".format(n)
-        return render_template('6-number_odd_or_even.html', n=text)
-    else:
-        text = "{} is odd".format(n)
-        return render_template('6-number_odd_or_even.html', n=text)
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """reload storage"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
