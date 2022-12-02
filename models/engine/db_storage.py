67bd4fc557dd58fe1ae7f4ccabbbac2f87178d0f
root <you@example.com>
2022-12-02 02:54:03 -0800

    add a new method for reload a session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,3 +66,7 @@ class DBStorage:
         sessionf = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sessionf)
         self.__session = Session()
+
+    def close(self):
+        """close session database"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,3 +70,7 @@ class FileStorage:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             del self.__objects[key]
             self.save()
+
+    def close(self):
+        """close session database"""
+        self.reload()
