5e9df59870a74b6847289a405c14e08b0aee674e
root <you@example.com>
2022-12-02 03:35:04 -0800

    update state for db storage

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -13,16 +14,18 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all,delete", backref="state")
 
-    @property
-    def cities(self):
-        """  getter attribute cities that returns the list of
-        City instances with state_id equals to the current State.id """
-        from models import storage
-        allcities = storage.all(City)
-        c = {}
-        for k, v in allcities.items():
-            if v.state_id == self.id:
-                c[k] = v
-        return c
+    if (getenv("HBNB_TYPE_STORAGE") == 'db'):
+        cities = relationship("City", cascade="all,delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """  getter attribute cities that returns the list of
+            City instances with state_id equals to the current State.id """
+            from models import storage
+            allcities = storage.all(City)
+            c = {}
+            for k, v in allcities.items():
+                if v.state_id == self.id:
+                    c[k] = v
+            return c
